c366a53657bb49bb4b7b968617de1716f918d21a
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-01-19 22:19:41 -0300

    Criando as classes de personagens 2

--- javaScript_Css/script.js
+++ javaScript_Css/script.js
@@ -1,5 +1,20 @@
-let char = new Knight('Willian');
+let char1 = new Knight('Willian');
+let char2 = new Sorcerer('Arthur');
+let monster = new LittleMonster();
+let bigmonster = new BigMonster();
 
-console.log(char.name);
-console.log(char.life);
-console.log(char.attack);
+console.log(char1.name);
+console.log(char1.life);
+console.log(char1.attack);
+
+console.log(char2.name);
+console.log(char2.life);
+console.log(char2.attack);
+
+console.log(monster.name);
+console.log(monster.life);
+console.log(monster.attack);
+
+console.log(bigmonster.name);
+console.log(bigmonster.life);
+console.log(bigmonster.attack);
--- javaScript_classes/script_classes.js
+++ javaScript_classes/script_classes.js
@@ -42,3 +42,22 @@ class Sorcerer extends Character{
     }
 }
 
+class LittleMonster extends Character{
+    constructor(){
+        super('Little Monster');
+        this.life = 40;
+        this.attack = 4;
+        this.defense = 4;
+        this.maxLife = this.life;
+    }
+}
+
+class BigMonster extends Character{
+    constructor(){
+        super('Big Monster');
+        this.life = 120;
+        this.attack = 16;
+        this.defense = 6;
+        this.maxLife = this.life;
+    }
+}
